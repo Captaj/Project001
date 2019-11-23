f49b545e1e1b2c8a08149d580e5c22d0ca73a0fc
Adam <master_aj_@hotmail.com>
2019-11-22 19:21:58 -0500

    Rudimentary character and animations
    It has base animations of walk and idle. There is a placeholder character. It doesn't look great when walking backward.

--- Assets/Scrips/Dash.cs
+++ Assets/Scrips/Dash.cs
@@ -9,19 +9,24 @@ public class Dash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if ((Input.GetKeyDown(KeyCode.LeftShift)) && amount > 0)
+		//checks for shift key input, and sees if player has any dashes left.
+        if ((Input.GetKeyDown(KeyCode.LeftShift)) && amount > 0)
 		{
-			Vector3 ray = transform.TransformDirection(Vector3.forward);
+			//shoots a ray forward the direction and distance of the dash to see if something is there.
+            Vector3 ray = transform.TransformDirection(Vector3.forward);
 			RaycastHit hit;
-			if (Physics.Raycast(this.transform.position, ray, out hit, 5))
+			//if it hits something before getting to the distance, it teleports a set distance before the point.
+            if (Physics.Raycast(this.transform.position, ray, out hit, 5))
             {
 				this.transform.position = hit.point -= this.transform.forward * 1;
 			}
+            //If it doesn't hit anything, it teleports the set distance. 
             else
 			{
 				this.transform.position = this.transform.forward * 5;
 			}
-			amount -= 1;
+			//each time it teleports, it takes away a set number of charges.
+            amount -= 1;
 
 		}
 		
--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5;
     private Rigidbody rb;
 
-    private Vector3 moveinput;
+    private Vector3 moveInput;
     private Vector3 moveVelocity;
     private Camera cam;
+    public Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +24,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveinput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical")); //getaxisraw for precise movement, instantly stops movement when keys stop being pressed
-        moveVelocity = moveinput * moveSpeed;
-        
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical")); //getaxisraw for precise movement, instantly stops movement when keys stop being pressed
+        moveVelocity = moveInput * moveSpeed;
+
+        animator.SetFloat("Speed", moveVelocity.sqrMagnitude);
+
+
         Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition); //creates line going from camera to mouse position
         Plane groundplane = new Plane(Vector3.up, Vector3.zero);
         float raylength;
