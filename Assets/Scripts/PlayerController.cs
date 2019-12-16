21cada5342d86cccbc1729d3c637a23582f1d282
Adam <57605169+Captaj@users.noreply.github.com>
2019-12-15 22:50:58 -0500

    Movement Rework 0.1
    Just transfering computers.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 5f;
+
+    private PlayerMotor motor;
+
+    [SerializeField]
+    private float speedMod = 1f;
+
+    [SerializeField]
+    private float jumpForce = 100f;
+
+    [SerializeField]
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _yMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = Vector3.right * _xMov;
+        Vector3 _moveVertical = Vector3.forward * _yMov;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical) * playerSpeed * speedMod;
+
+        motor.Move(_velocity);
+
+        Vector3 _jumpForce = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            _jumpForce = Vector3.up * jumpForce;
+        }
+
+        motor.Jump(_jumpForce);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 jumpForce = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Jump(Vector3 _jumpForce)
+    {
+        jumpForce = _jumpForce;
+    }
+
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformJump();
+    }
+
+    void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (jumpForce != Vector3.zero)
+        {
+            
+        }
+    }
+
+    public void PerformJump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
+
+}
